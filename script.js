92e669763401c54f6c1bba164bc620afc65dde31
puja <proy87830@gmail.com>
2025-07-12 13:07:11 +0530

    all change

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
+function Delete() {
+  document.getElementById("display").value = display.value.toString().slice(0, -1);
+}
 
+function calculate() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (e) {
+    document.getElementById("display").value = "Error";
+  }
+}
